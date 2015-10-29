023f88c0efc3967943d4cc780d9cfa6fa52ab050
Josephliao1991 <gn02239440@gmail.com>
2015-10-29 14:52:58 +0800

    no message

--- server.js
+++ server.js
@@ -1,22 +1,88 @@
 var cluster = require('cluster');
-var http = require('http');
-var numCPUs = require('os').cpus().length;
+var numCPUs = require('os').cpus().length;  //Get Number Of CPU
+require('should');
+
+
+var express = require('express');
+var app     = express()
+
+var index = 1;
+index.should.equal(index);
 
 if (cluster.isMaster) {
-    console.log('[master] ' + "start master...");
+  console.log('[master] ' + "start master...");
 
-    for (var i = 0; i < numCPUs; i++) {
-         cluster.fork();
+  //Check Index Per Sec.
+  setInterval(function() {
+    console.log("Now Worker : Worker", index);
+  }, 1000);
+
+  // Count requests
+  function messageHandler(msg) {
+    if (msg.cmd && msg.cmd == 'notifyRequest') {
+      if (numCPUs == index) {
+        index = 1
+        return
+      }
+      index += 1;
     }
+  }
 
-    cluster.on('listening', function (worker, address) {
-        console.log('[master] ' + 'listening: worker' + worker.id + ',pid:' + worker.process.pid + ', Address:' + address.address + ":" + address.port);
-    });
+  // Start workers and listen for messages containing notifyRequest
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  //Listen For Worker Start Process
+  cluster.on('listening', function (worker, address) {
+    console.log('[master] ' + 'listening: worker' + worker.id + ',pid:' + worker.process.pid + ', Address:' + address.address + ":" + address.port);
+  });
+
+  // Listen For Dying Worker
+  cluster.on('exit', function (worker) {
+    // Replace the dead worker,
+    // we're not sentimental
+    console.log('[worker] Worker%d died :(', worker.id);
+    cluster.fork();
+  });
+
+  //Set On Message Listener
+  Object.keys(cluster.workers).forEach(function(id) {
+    cluster.workers[id].on('message', messageHandler);
+  });
 
 } else if (cluster.isWorker) {
-    console.log('[worker] ' + "start worker ..." + cluster.worker.id);
-    http.createServer(function (req, res) {
-        console.log('worker'+cluster.worker.id);
-        res.end('worker'+cluster.worker.id+',PID:'+process.pid);
-    }).listen(8080);
+  //Check Worker Is Start
+  console.log('[worker] ' + "start worker ..." + cluster.worker.id);
+  var workerId = cluster.worker.id
+
+  //Set Time Interval To Check Index Per Sec.
+  setInterval(function() {
+
+    if (index == workerId) {
+      index += 1
+      // notify master about the request
+      // process.send({ cmd: 'notifyRequest' });
+    }
+  }, 1000);
+
+
+
+//Create Server On Each Child.Process, And Cluster Will Auto LoadBlance
+  //Create Server, Listen 8080 Port
+  // app.get('/',function (req, res) {
+  //   // body...
+  //   console.log('worker'+cluster.worker.id);
+  //   res.end('worker'+cluster.worker.id+',PID:'+process.pid);
+  // })
+  //
+  // app.listen(8080,function (error) {
+  //   // body...
+  //   if (error) {
+  //     console.log('[worker] Fail, Worker%d Can Not Start Server',cluster.worker.id);
+  //   }
+  //   console.log('[worker] Success, Worker%d Start Server',cluster.worker.id);
+  //
+  // })
+
 }
